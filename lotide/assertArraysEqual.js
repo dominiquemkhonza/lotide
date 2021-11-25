21455bf306fede2ee4c8914373c3780e73beecd9
Dominique Mkhonza <dominique__m@hotmail.com>
2021-11-25 00:38:29 +0000

    Lotide project

---
+++ lotide/assertArraysEqual.js
@@ -0,0 +1,16 @@
+const {eqArrays} = require('./eqArrays');
+
+/* Take two arrays and console.log appropriate message to the console */
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅ assertation passed : ${array1}  === ${array2}`); 
+    } else {
+    console.log(`🔴 assertation failed : ${array1} !== ${array2}`); 
+    }
+};
+
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertArraysEqual(eqArrays(["1", "6", "3"], ["1", "2", "3"]), true);
+
+module.exports = assertArraysEqual;
