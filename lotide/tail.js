e77b36c78e035f844ee595405adb0fc31a4c7a48
Dominique Mkhonza <dominique__m@hotmail.com>
2021-11-26 04:17:31 +0000

    Function tail returns everything except for the first item of an array

--- lotide/tail.js
+++ lotide/tail.js
@@ -1,12 +1,23 @@
+
+/* 
+      a) create function "tail" which returns everything except for the first item of an array
+      b) do not modify the array that is passed in 
+*/
+
+////// assertEqual code //////
 const {assertEqual} = require('./assertEqual');
+/////////////////////////////
+
+const words = ["profound", "metaphysical", "truths", "are", "present", "in", "nature"];
 
 const tail = function(array) {
-  return array.length > 2 ? array.slice(1) : [];
+  let tailArray = [];
+  for (let i = 1; i <array.length; i++) {
+    tailArray.push(array[i]);
+  }
+  return tailArray;
 };
 
+tail(words);
+assertEqual(words.length, 5);
 module.exports = tail;
-module.exports = tail;
-//*  test assertations //*
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
