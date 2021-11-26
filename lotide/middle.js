f5ab30c7dba2a8554562ec2b9f473496e8bdf7a6
Dominique Mkhonza <dominique__m@hotmail.com>
2021-11-26 05:39:26 +0000

    Middle function returns the most middle elements of a given array

---
+++ lotide/middle.js
@@ -0,0 +1,33 @@
+
+/* 
+MIDDLE function
+  a) should take in an array
+  b) return the most-middle elements of a given-array
+*/ 
+
+const {eqArrays} = require('./eqArrays');
+const {assertArraysEqual} = require('./assertArraysEqual');
+
+/////// actual function ///////
+const middle = function(array) {
+  if (array.length <= 2) {
+    return [];
+  } else if (array.length % 2 === 0) {
+    let first = array.length / 2 - 1;
+    let second = array.length / 2;
+    return [array[first], array[second]];
+  } else {
+    return [array[Math.floor(array.length /2)]];
+  }
+};
+
+module.exports = middle;
+
+
+////// test code //////
+console.log(middle([1, 2, 3, 4]));
+console.log(middle([1, 2, 3, 4, 5, 6]));
+console.log(middle([1, 2, 3]));
+console.log(middle([1, 2, 3, 4, 5]));
+console.log(middle([1]));
+console.log(middle([1, 2]));
