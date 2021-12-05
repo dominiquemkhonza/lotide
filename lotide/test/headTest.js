d31d50dc8d545ea673d7d6e5cbf913f126719ccc
Dominique Mkhonza <dominique__m@hotmail.com>
2021-12-05 21:50:26 +0000

    Edited headTest to work with Chai

--- lotide/test/headTest.js
+++ lotide/test/headTest.js
@@ -1,23 +1,15 @@
 const head = require('../head');
-const chai = require ('chai');  
-const assert = chai.assert;
+const assert = require ('chai').assert;
 
-assert(head([111, 777, 333]), 777);
-assert(head(["Mi", "chiamo", "Dominique"]), "Mi");
-assert(head([5,6,7]), 5);
-assert(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
 describe("#head", () => {
-  it("returns 2 for [2, 4, 6", () => {
-    assert.strictEqual(head([2, 4, 6]), 2);
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
   });
 
-  it("returns 'Mi' for ['Mi', 'chiamo', 'Dominique'", () => {
-    assert.strictEqual(head(["Mi, chiamo", "Dominique"]),"Mi");
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
   });
 
-  it("returns 'undefined' for []", () => {
-    assert.strictEqual(head([]), undefined);
-  });
 
 });
